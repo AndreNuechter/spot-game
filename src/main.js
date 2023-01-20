b730d4c53d56100c0bd270bdd86b5c8fa346f966
Andre Nuechter <andre_nuechter@hotmail.de>
2023-01-21 00:54:16 +0100

    Reset board when players change and some refactoring

--- src/main.js
+++ src/main.js
@@ -1,69 +1,78 @@
 import './js/service-worker-init.js';
 import './js/wakelock.js';
 
-// https://codepen.io/AndreNuechter/pen/bmJLVr
-
-// TODO icons (fontawesome)
-// TODO refactor
+// TODO rework colors (see manifest and index.html too)
+// TODO refactor...rm inline styles, rm compound assignments, 
 
 window.addEventListener('DOMContentLoaded', () => {
-    const id = (id) => document.getElementById(id);
-    const cls = (cls) => document.getElementsByClassName(cls);
-    const start = id('start');
+    const getElementById = (id) => document.getElementById(id);
+    const getElementByClass = (cls) => document.getElementsByClassName(cls);
+    const startBtn = getElementById('start');
     const boardLen = 7;
     const board = new Array(Math.pow(boardLen, 2)).fill(0);
-    const slct = cls('slct');
-    const plrs = [
-        new Player(slct[0].style.color, 'user', 0),
-        new Player(slct[2].style.color, 'inactive', boardLen - 1),
-        new Player(slct[3].style.color, 'robot', Math.pow(boardLen, 2) - 1),
-        new Player(
-            slct[1].style.color,
-            'inactive',
-            Math.pow(boardLen, 2) - boardLen,
-        ),
+    const boardObject = getElementById('board');
+    const playerSelects = getElementByClass('slct');
+    const players = [
+        // TODO colors are duplicated in css
+        new Player('#3399ff', 'user', 0),
+        new Player('#ff5050', 'inactive', boardLen - 1),
+        new Player('#ff9900', 'inactive', Math.pow(boardLen, 2) - boardLen),
+        new Player('#009900', 'robot', Math.pow(boardLen, 2) - 1),
     ];
-    const gameOver = id('game-over-indicator');
-    const circles = [];
+    const gameOverModal = getElementById('game-over-indicator');
+    const boardCells = [];
     const boardWidth = 7;
-    const boardObject = id('board');
+    const cellSize = 32;
     const circleTmpl = document.createElementNS(
         'http://www.w3.org/2000/svg',
         'circle',
     );
-    let actives;
+    let idsOfActivePlayers;
     let turn;
 
-    circleTmpl.setAttribute('fill', 'beige');
-    circleTmpl.setAttribute('stroke', 'black');
-    circleTmpl.setAttribute('stroke-width', '2');
     circleTmpl.setAttribute('r', '13');
 
     for (let i = 1; i <= boardWidth; i += 1) {
         for (let j = 1; j <= boardWidth; j += 1) {
             const circle = circleTmpl.cloneNode(true);
 
-            circle.setAttribute('cx', 32 * j);
-            circle.setAttribute('cy', 32 * i);
+            circle.setAttribute('cx', cellSize * j);
+            circle.setAttribute('cy', cellSize * i);
 
-            circles.push(circle);
+            boardCells.push(circle);
         }
     }
 
-    boardObject.append(...circles);
+    boardObject.append(...boardCells);
 
-    // Register eventlisteners for plr-selection
-    for (let plr of slct) {
-        changeRole(plr);
+    // register eventlisteners for player-selection
+    for (let playerSelect of playerSelects) {
+        changeRole(playerSelect);
     }
 
-    start.onclick = () => {
-        start.innerText = 'Restart Game';
-        actives = [];
-        plrs.forEach((plr, i) => {
-            if (plr.state !== 'inactive') actives.push(i);
-        });
-        setBoard(actives);
+    function resetBoard() {
+        startBtn.innerText = 'Start Game';
+        mainClassList.remove('game-is-running');
+        clearBoard();
+        turn = undefined;
+    }
+
+    const mainClassList = document.querySelector('main').classList;
+
+    startBtn.onclick = () => {
+        if (!mainClassList.contains('game-is-running')) {
+            startBtn.innerText = 'Restart Game';
+            mainClassList.add('game-is-running');
+        }
+
+        idsOfActivePlayers = players.reduce((ids, currentPlayer, i) => {
+            if (currentPlayer.state !== 'inactive') {
+                ids.push(i);
+            }
+            return ids;
+        }, []);
+        turn = 0;
+        setBoard(idsOfActivePlayers);
     };
 
     function Player(color, state, start) {
@@ -107,13 +116,16 @@ window.addEventListener('DOMContentLoaded', () => {
                 isPieceNotAtRBorder ? piece - 2 * boardLen + 1 : -1,
                 isPieceNotAtRBorder ? piece + 2 * boardLen + 1 : -1,
             ],
-            findFreeCells = (val) => 0 <= val && val < board.length && board[val] === 0,
             neighbors = neighborCells.filter(findFreeCells),
             oneOff = cellsOneOff.filter(findFreeCells);
         return {
             nextTo: neighbors,
             oneOff: oneOff,
         };
+    }
+
+    function findFreeCells(val) {
+        return 0 <= val && val < board.length && board[val] === 0;
     }
 
     function getEnemyNeighbors(piece) {
@@ -131,23 +143,23 @@ window.addEventListener('DOMContentLoaded', () => {
             ];
         return neighborCells.filter((cell) =>
             0 <= cell && cell < board.length && board[cell] !== 0 &&
-            board[cell] !== actives[turn] + 1
+            board[cell] !== idsOfActivePlayers[turn] + 1
         );
     }
 
     function clearCell(cell) {
         board[cell] = 0;
-        circles[cell].style.fill = 'beige';
-        circles[cell].classList = circles[cell].onclick = '';
+        boardCells[cell].style.fill = 'beige';
+        boardCells[cell].classList = boardCells[cell].onclick = '';
     }
 
     function clearBoard() {
-        gameOver.style.zIndex = -1;
-        plrs.forEach((plr, i) => {
+        gameOverModal.style.zIndex = -1;
+        players.forEach((plr, i) => {
             plr.pieces.length = 0;
-            id('plr' + (i + 1)).children[1].textContent = '';
+            getElementById(`plr${i + 1}`).children[1].textContent = '';
         });
-        for (let i = 0; i < circles.length; i += 1) {
+        for (let i = 0; i < boardCells.length; i += 1) {
             clearCell(i);
         }
     }
@@ -166,97 +178,113 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function endTurn() {
-        // log pieces at turn's end
-        // log('player', actives[turn] + 1, 'now has', plrs[actives[turn]].pieces);
+        if (turn === undefined) { 
+            resetBoard();
+            return;
+        }
+
         // remove turn highlighting
-        id('plr' + (actives[turn] + 1)).classList.remove('active');
+        getElementById('plr' + (idsOfActivePlayers[turn] + 1)).classList.remove('active');
+
         // advance turn
-        if (turn < actives.length - 1) turn += 1;
-        else turn = 0;
+        if (turn < idsOfActivePlayers.length - 1) {
+            turn += 1;
+        } else {
+            turn = 0;
+        }
+
         // adjust piece counts
-        actives.forEach((e) => id('plr' + (e + 1)).children[1].textContent = plrs[e].pieces.length);
+        idsOfActivePlayers.forEach((playerId) => {
+            getElementById(`plr${playerId + 1}`).children[1].textContent = players[playerId].pieces.length;
+        });
+
         // stop game when board is full or only one player w pieces remains
         if (
             board.filter((cell) => cell === 0).length === 0 ||
-            plrs.filter((e) => e.pieces.length > 0).length <= 1
+            players.filter((e) => e.pieces.length > 0).length <= 1
         ) {
-            // find plr placement and communicate end of game
-            const places = actives.map((i) => [i + 1, plrs[i].pieces.length]).sort((
-                a,
-                b,
-            ) => b[1] - a[1]);
-            places.forEach((pl, i) => {
-                id('plr' + (pl[0])).children[1].textContent = getPlaceStr(i);
-            });
-            gameOver.style.zIndex = 1;
-        } else move();
+            // rank players and communicate end of game
+            idsOfActivePlayers
+                .map((id) => ({
+                    playerId: id + 1,
+                    pieces: players[id].pieces.length
+                }))
+                .sort(({ pieces: a }, { pieces: b }) => b - a)
+                .forEach(({ playerId }, i) => {
+                    getElementById(`plr${playerId}`).children[1].textContent = getPlaceStr(i);
+                });
+            gameOverModal.style.zIndex = 1;
+            document.querySelector('main').classList.remove('game-is-running');
+        } else {
+            move();
+        }
     }
 
     function humanMove(options) {
-        const plr = plrs[actives[turn]];
+        const plr = players[idsOfActivePlayers[turn]];
         // allow selection of a piece
-        options.forEach((o) => {
+        options.forEach((option) => {
             // change cursor for selectable pieces
-            circles[o.piece].style.cursor = 'pointer';
-            circles[o.piece].onclick = () => {
+            boardCells[option.piece].style.cursor = 'pointer';
+            boardCells[option.piece].onclick = () => {
                 // allow deselection of a piece
-                if (circles[o.piece].classList.contains('selected')) {
+                if (boardCells[option.piece].classList.contains('selected')) {
                     // remove highlighting
-                    circles[o.piece].classList.remove('selected');
+                    boardCells[option.piece].classList.remove('selected');
                     // re-allow selection
                     humanMove(options);
                     // remove listeners for move and cursor-highlighting
-                    [...o.moves.nextTo, ...o.moves.oneOff].forEach((e) => {
-                        circles[e].onclick = '';
-                        circles[e].style.cursor = 'initial';
+                    [...option.moves.nextTo, ...option.moves.oneOff].forEach((e) => {
+                        boardCells[e].onclick = '';
+                        boardCells[e].style.cursor = 'initial';
                     });
                 } else {
                     const helper = (e) => {
-                        [...o.moves.nextTo, ...o.moves.oneOff].forEach((e2) => {
-                            circles[e2].onclick = '';
-                            circles[e2].style.cursor = 'initial';
+                        [...option.moves.nextTo, ...option.moves.oneOff].forEach((e2) => {
+                            boardCells[e2].onclick = '';
+                            boardCells[e2].style.cursor = 'initial';
                         });
-                        board[e] = actives[turn] + 1;
-                        circles[e].style.fill = plr.color;
+                        board[e] = idsOfActivePlayers[turn] + 1;
+                        boardCells[e].style.fill = plr.color;
                         // check for neigbouring enemy pieces and turn them over
                         getEnemyNeighbors(e).forEach((gained) => {
-                            const owner = plrs[board[gained] - 1];
+                            const owner = players[board[gained] - 1];
                             // remove piece from current owner and give it to player
                             plr.pieces.push(
                                 ...owner.pieces.splice(owner.pieces.indexOf(gained), 1),
                             );
                             // update internal board state
-                            board[gained] = actives[turn] + 1;
+                            board[gained] = idsOfActivePlayers[turn] + 1;
                             // update display
-                            circles[gained].style.fill = plr.color;
+                            boardCells[gained].style.fill = plr.color;
                         });
                         endTurn();
                     };
                     // highlight selected piece
-                    circles[o.piece].classList.add('selected');
+                    boardCells[option.piece].classList.add('selected');
                     // remove eventlisteners and cursor-highlighting from other possible selection targets
                     plr.pieces.forEach((j) => {
-                        if (j !== o.piece) {
-                            circles[j].onclick = '';
-                            circles[j].style.cursor = 'initial';
+                        if (j !== option.piece) {
+                            boardCells[j].onclick = '';
+                            boardCells[j].style.cursor = 'initial';
                         }
                     });
                     // listen for click on possible target
-                    o.moves.nextTo.forEach((e) => {
-                        circles[e].style.cursor = 'pointer';
-                        circles[e].onclick = () => {
-                            circles[o.piece].onclick = circles[o.piece].classList = '';
-                            circles[o.piece].style.cursor = 'initial';
+                    option.moves.nextTo.forEach((e) => {
+                        boardCells[e].style.cursor = 'pointer';
+                        boardCells[e].onclick = () => {
+                            boardCells[option.piece].onclick = boardCells[option.piece].classList = '';
+                            boardCells[option.piece].style.cursor = 'initial';
                             plr.pieces.push(e);
                             helper(e);
                         };
                     });
-                    o.moves.oneOff.forEach((e) => {
-                        circles[e].style.cursor = 'pointer';
-                        circles[e].onclick = () => {
-                            clearCell(o.piece);
-                            circles[o.piece].style.cursor = 'initial';
-                            plr.pieces[plr.pieces.indexOf(o.piece)] = e;
+                    option.moves.oneOff.forEach((e) => {
+                        boardCells[e].style.cursor = 'pointer';
+                        boardCells[e].onclick = () => {
+                            clearCell(option.piece);
+                            boardCells[option.piece].style.cursor = 'initial';
+                            plr.pieces[plr.pieces.indexOf(option.piece)] = e;
                             helper(e);
                         };
                     });
@@ -265,10 +293,13 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    function wait(ms) {
+        return new Promise((r) => setTimeout(r, ms));
+    } 
+
     async function machineMove(options) {
-        const plr = plrs[actives[turn]],
-            payoffs = [],
-            wait = (ms) => new Promise((r, j) => setTimeout(r, ms));
+        const plr = players[idsOfActivePlayers[turn]],
+            payoffs = [];
         let most = [0, []],
             secMost = [],
             thrdMost;
@@ -332,28 +363,28 @@ window.addEventListener('DOMContentLoaded', () => {
             move = candidates[1][id];
         // wait a bit and highlight selected piece
         await wait(200);
-        circles[move.origin].classList.add('selected');
+        boardCells[move.origin].classList.add('selected');
         // wait a bit more and highlight target cell
         await wait(400);
-        circles[move.target].style.stroke = 'Yellow';
+        boardCells[move.target].style.stroke = 'Yellow';
         // wait even more and make move
         await wait(500);
         [move.target, ...move.bounty].forEach((cell) => {
             // Place piece in chosen cell
             if (board[cell] === 0) plr.pieces.push(cell);
             else {
-                const owner = plrs[board[cell] - 1];
+                const owner = players[board[cell] - 1];
                 // Flip neighboring enemy pieces
                 plr.pieces.push(...owner.pieces.splice(owner.pieces.indexOf(cell), 1));
             }
             // Update board and display
-            board[cell] = actives[turn] + 1;
-            circles[cell].style.fill = plr.color;
+            board[cell] = idsOfActivePlayers[turn] + 1;
+            boardCells[cell].style.fill = plr.color;
         });
         // wait again and undo highlighting of move
         await wait(200);
-        circles[move.origin].classList.remove('selected');
-        circles[move.target].style.stroke = 'black';
+        boardCells[move.origin].classList.remove('selected');
+        boardCells[move.target].style.stroke = 'black';
         // when jumped, remove piece from origin
         if (move.type === 0) {
             clearCell(move.origin);
@@ -363,18 +394,18 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function move() {
+        if (turn === undefined) return;
         // highlight active player
-        id('plr' + (actives[turn] + 1)).classList.add('active');
-        // log current pieces
-        // log('player', actives[turn] + 1, 'has', plrs[actives[turn]].pieces);
+        getElementById('plr' + (idsOfActivePlayers[turn] + 1)).classList.add('active');
         // go thru players' pieces, see if there're moves and bundle em up for later use
-        const plr = plrs[actives[turn]],
+        const plr = players[idsOfActivePlayers[turn]],
             options = plr.pieces
                 .map((piece) => ({ piece: piece, moves: getMoves(piece) }))
                 .filter((option) =>
                     option.moves.nextTo.length > 0 || option.moves.oneOff.length > 0
                 );
-        if (options.length === 0 || plr.state === 'inactive') endTurn();
+
+        if (options.length === 0) endTurn();
         else if (plr.state === 'user') humanMove(options);
         else if (plr.state === 'robot') machineMove(options);
     }
@@ -383,12 +414,11 @@ window.addEventListener('DOMContentLoaded', () => {
         if (actives.length < 2) return;
         clearBoard();
         actives.forEach((e) => {
-            circles[plrs[e].start].style.fill = plrs[e].color;
-            plrs[e].pieces.push(plrs[e].start);
-            board[plrs[e].start] = e + 1;
-            id('plr' + (e + 1)).children[1].textContent = plrs[e].pieces.length;
+            boardCells[players[e].start].style.fill = players[e].color;
+            players[e].pieces.push(players[e].start);
+            board[players[e].start] = e + 1;
+            getElementById('plr' + (e + 1)).children[1].textContent = players[e].pieces.length;
         });
-        turn = 0;
         move();
     }
 
